85a4369c55b7cd988c44456d1aed7a3d0b853fd0
soner3 <sonerastan@icloud.com>
2026-02-22 18:09:52 +0100

    feat: Add and integrate Wire-generated dependency injection container.

--- example/main.go
+++ example/main.go
@@ -15,12 +15,17 @@ limitations under the License.
 */
 package main
 
-func main() {
-	// container, err := InitializeContainer()
-	// if err != nil {
-	// 	fmt.Printf("Failed to initialize DI container: %v\n", err)
-	// 	os.Exit(1)
-	// }
+import (
+	"fmt"
+	"os"
+)
 
-	// container.UserService.PrintUser()
+func main() {
+	container, err := InitializeContainer()
+	if err != nil {
+		fmt.Printf("Failed to initialize DI container: %v\n", err)
+		os.Exit(1)
+	}
+
+	container.UserService.PrintUser()
 }
